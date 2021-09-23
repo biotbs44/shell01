527f49380332cc179ffb672c7626517b97a7db4a
biotbs44 <immchaebol@naver.com>
2021-09-23 12:06:38 +0900

    finish

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat /etc/passwd | sed '/\#/d' | awk 'NR%2 == 0 {print$0}' \
-| cut -f 1 -d ':' | rev | sort -f -r | \
-awk 'NR>='${FT_LINE1}' && NR<='${FT_LINE2}' {print;}' | \
-tr '\n' ',' | sed -e 's/, */, /g' -e 's/.$//' -e 's/.\{3\}$/./'
+cat /etc/passwd | grep -v -e "#" \
+| awk "NR % 2 == 0" | cut -f 1 -d ':' | rev \
+| sort -rn | awk "FNR>=$FT_LINE1 && FNR<=$FT_LINE2" \
+| sed 's/$/,/' | tr '\n' ' ' | sed 's/, *$/\./'
